de0006351b0bae6bf16310f3beda381069c0f999
Nicholas-Clavette <78097185+Nicholas-Clavette@users.noreply.github.com>
2021-04-13 14:05:11 -0400

    Update server.py

--- server.py
+++ server.py
@@ -11,16 +11,39 @@ import time
 from tkinter import *
 import socket
 import _pickle as pickle
+import threading
+import sys
+
+#try:
+#    import thread
+#except ImportError:
+#    import _thread as thread
 
 sense = SenseHat()
-sense.clear()
+
+sense.set_imu_config(True, True, True)
+
+orientation = None
+
+def orientation_thread():
+    global orientation
+    global ap
+    while(True):
+        orientation = sense.get_orientation_degrees()
+
+data_thread = threading.Thread(target=orientation_thread)
+data_thread.daemon = True
+data_thread.start()
+time.sleep(1)
 
 # SERVER SENDING DATA
-host = '192.168.1.13'
-port = 9607
+host = '192.168.1.17'
+port = 9606
 s = socket.socket()
 s.bind((host, port))
 s.listen(10)
+
+
 while True:
     try:
         conn, addr = s.accept()
@@ -30,12 +53,12 @@ while True:
                 for event in sense.stick.get_events():
                     if event.action == "held":
                         if event.direction == "middle":
-                            gyro = sense.get_gyroscope()
-                            data = pickle.dumps(gyro, -1)
+                            data = pickle.dumps(orientation, -1)
                             conn.sendall(data)
     except:
         conn.close()
         break
         
 s.close()
+sys.exit()
 
